a8301a6cd6ed349868ba770698ed4ed691076b15
Brian Ho <bh1525@nyu.edu>
2023-09-24 03:04:15 -0500

    mobile

---
+++ .history/components/Description_20230923200410.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import { Explanation } from './Explanation'
+export const Description = () => {
+	return (
+		<>
+			<div className='description work_in_progress'>
+				<div className='blue_gradient small' id='circle_5'></div>
+				<div className='circle_blue_halo' id='circle_6'>
+					<div className='circle_small_blue'></div>
+				</div>
+				<div className='circle_small_blue' id='circle_7'></div>
+				<p>I</p>
+				<h2 className='title title_001'>
+					CHARTING <br />
+					UNCERTAINTY
+				</h2>
+				<p className='text'>
+					What you are reading is a work-in-progress and an ongoing
+					experiment. Part e-book, part digital garden, part
+					collaborative project, it explores a pervasive feature of
+					our existence: the nature of Uncertainty.
+				</p>
+			</div>
+			<div className='description web_of_uncertainty'>
+				<p>II</p>
+				<h2 className='title title_002'>
+					UNCERTAINTY&apos;S <br></br>WEB
+				</h2>
+				<div className='circle_small_blue' id='circle_8'></div>
+				<div className='circle_orange_halo' id='circle_9'></div>
+			</div>
+			<div className='description trivial_uncertainty'>
+				<div className='circle_small_blue' id='circle_10'></div>
+				<div className='circle_orange_halo' id='circle_11'></div>
+				<div className='circle_small_blue' id='circle_12'></div>
+				<p className='text'>
+					This ambiguous state may appear trivial, but it is nothing
+					but. It can trigger anxiety and doubt, affect our
+					decision-making, cause market-moves, and shift the
+					geo-strategic balance of power.
+				</p>
+				<div className='blue_gradient small' id='circle_13'></div>
+			</div>
+			<div className='description shape_uncertainty'>
+				<div className='blue_gradient' id='circle_14'></div>
+				<p>III</p>
+				<h2 className='title title_003'>SHAPING</h2>
+				<p className='text'>
+					Our current understanding is fractured and pre-paradigmatic.
+					Different disciplines define uncertainty in mutually
+					incompatible ways. This is my attempt to re-draw the shape
+					of uncertainty.
+				</p>
+			</div>
+			<div className='description exploring_uncertainty'>
+				<p>IV</p>
+				<h2 className='title title_004'>
+					BETWEEN THOUGHT <br></br>AND THEATER
+				</h2>
+				<p className='text'>
+					It is the ongoing diary of my first-principles exploration
+					into the unknown. Although I am not a scholar, it is quite
+					scholarly in places. Although I am not an artist, it is
+					quite artistic in others.
+				</p>
+			</div>
+			<div className='description tangible_abstract'>
+				<p className='text left'>
+					It is both{' '}
+					<span className='italic_text'>
+						<i>playful</i>
+					</span>{' '}
+					and <span className='bold_text'>serious</span>,
+				</p>
+				<p className='text right'>abstract and concrete.</p>
+				<p className='text center'>
+					Its structure is fractal, but you can read it like a linear
+					book,
+					<br /> or freely skip around the knowledge graph.
+				</p>
+			</div>
+			<Explanation />
+		</>
+	)
+}
---
+++ .history/components/Explanation_20230922175330.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import { MagicCard } from './magicui/magic-card'
+import { MagicContainer } from './magicui/magic-card'
+import Image from 'next/image'
+
+export const Explanation = () => {
+	return (
+		<>
+			<div className='explanation'>
+				<h2 className='title'>How To Navigate:</h2>
+				<MagicContainer
+					borderWidth={3}
+					className={
+						'flex mx-auto w-[30px] items-center flex-col lg:h-[250px] lg:w-screen lg:flex-row lg:mx-auto lg:flex-wrap gap-4 mc'
+					}
+				>
+					<MagicCard className='flex max-w-1/3 p-4 cursor-pointer gap-2 flex-col items-center justify-center overflow-hidden p-20 shadow-2xl'>
+						<p className='z-10 whitespace-nowrap text-3xl font-medium text-white-800 dark:text-white-200'>
+							Opening Sequence
+						</p>
+						<Image
+							src='/images/cover.png'
+							className='explainer'
+							width={250}
+							height={150}
+							alt='Logo'
+						></Image>
+						<div className='pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_120%,rgba(120,119,198,0.3),rgba(255,255,255,0))]' />
+					</MagicCard>
+					<MagicCard
+						className='flex max-w-1/3 p-4 cursor-pointer flex-col gap-2 items-center justify-center overflow-hidden p-20 shadow-2xl'
+						spotlight
+					>
+						<p className='z-10 whitespace-nowrap text-3xl font-medium text-white-800 dark:text-white-200'>
+							Navigation
+						</p>
+						<Image
+							src='/images/layer1.png'
+							className='explainer'
+							width={250}
+							height={150}
+							alt='Logo'
+						></Image>
+
+						<div className='pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_120%,rgba(120,119,198,0.3),rgba(255,255,255,0))]' />
+					</MagicCard>
+					<MagicCard className='flex max-w-1/3 p-4 cursor-pointer gap-2 flex-col items-center justify-center overflow-hidden p-20 shadow-2xl'>
+						<p className='z-10 whitespace-nowrap text-3xl font-medium text-white-800 dark:text-white-200'>
+							Child Nodes
+						</p>
+						<Image
+							src='/images/layer1-hover.png'
+							className='explainer'
+							width={250}
+							height={150}
+							alt='Logo'
+						></Image>
+						<div className='pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_120%,rgba(120,119,198,0.3),rgba(255,255,255,0))]' />
+					</MagicCard>
+					<MagicCard className='flex max-w-1/3 p-4 cursor-pointer gap-2 flex-col items-center justify-center overflow-hidden p-20 shadow-2xl'>
+						<p className='z-10 whitespace-nowrap text-3xl font-medium text-white-800 dark:text-whtite-200'>
+							Next Layer
+						</p>
+						<Image
+							src='/images/layer1-hover.png'
+							className='explainer'
+							width={250}
+							height={150}
+							alt='Logo'
+						></Image>
+						<div className='pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_120%,rgba(120,119,198,0.3),rgba(255,255,255,0))]' />
+					</MagicCard>
+					<MagicCard className='flex max-w-1/3 p-4 cursor-pointer gap-2 flex-col items-center justify-center overflow-hidden p-20 shadow-2xl'>
+						<p className='z-10 whitespace-nowrap text-3xl font-medium text-white-800 dark:text-white-200'>
+							Reading Page
+						</p>
+						<Image
+							src='/images/layer2.png'
+							className='explainer'
+							width={250}
+							height={150}
+							alt='Logo'
+						></Image>
+						<div className='pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_120%,rgba(120,119,198,0.3),rgba(255,255,255,0))]' />
+					</MagicCard>
+					<MagicCard className='flex max-w-1/3 p-4 cursor-pointer gap-2 flex-col items-center justify-center overflow-hidden p-20 shadow-2xl'>
+						<p className='z-10 whitespace-nowrap text-3xl font-medium text-white-800 dark:text-white-200'>
+							Table of Contents
+						</p>
+						<Image
+							src='/images/layer1-hover.png'
+							className='explainer'
+							width={250}
+							height={150}
+							alt='Logo'
+						></Image>
+						<div className='pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_120%,rgba(120,119,198,0.3),rgba(255,255,255,0))]' />
+					</MagicCard>
+				</MagicContainer>
+			</div>
+		</>
+	)
+}
