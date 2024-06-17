a4933ad0f02d319338f97184f20bcd051fdcff12
Rcin <cindyhyf@qq.com>
2024-06-17 18:50:57 -0400

    up to date againgit add .

--- components/Marginalia/Marginalia.jsx
+++ components/Marginalia/Marginalia.jsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
-import styles from '../../src/app/readingpage.module.css'
-import { useEffect } from 'react'
+import styles from './marginalia.module.css'
+import { useState, useEffect } from 'react'
 import useTheme from '../../hooks/useThemes'
+import MarginaliaPopup from './MarginaliaPopup'
 
-export default function Marginalia({ id, username, content, picture, mode }) {
-	const [theme, toggleTheme] = useTheme()
+
+
+export default function Marginalia({ id, username, content, picture, theme }) {
+	const [popupOpen, setPopupOpen] = useState(false);
 	useEffect(() => {
 		randomizeMarg()
 	})
@@ -21,15 +24,16 @@ export default function Marginalia({ id, username, content, picture, mode }) {
 	}
 
 	return (
-		<div key={id} className={`marginalia ${mode}`}>
-			{/* <div className="marginalia__container"> */}
-			<p className={`marginalia_text ${mode}`}>{content}</p>
+		<div>
+		{popupOpen ? <MarginaliaPopup id={id} username={username} content={content} picture={picture} theme={theme} setPopupOpen={setPopupOpen} /> : null}
+		<div key={id} className={`${styles.marginalia} ${theme}`} onClick={setPopupOpen}>
+			<p className={`${styles.text} ${theme}`}>{content}</p>
 			{picture && (
-				<div className='marginalia_picture'>
-					<img src={picture} alt='marginalia picture' width={200} />
+				<div className={styles.picture}>
+					<Image src={picture} alt='marginalia picture' width={200} height={200}/>
 				</div>
 			)}
-			<p className='marginalia_username'>{username}</p>
+			<p className='marginalia_username'>- {username}</p>
 			{/* </div> */}
 			<style jsx global>{`
 				/*
@@ -63,10 +67,16 @@ export default function Marginalia({ id, username, content, picture, mode }) {
 
 				.marginalia_username {
 					font-size: 1rem;
+					font-family: var(--modern-font);
+
+					position: absolute;
+					right: 10%;
+					font-style: italic;
 				}
 
 				.marginalia_text {
 					font-size: 1rem;
+					font-family: var(--modern-font);
 				}
 
 				.marginalia_picture {
@@ -86,6 +96,7 @@ export default function Marginalia({ id, username, content, picture, mode }) {
 					}
 				}
 			`}</style>
+			</div>
 		</div>
 	)
 }
---
+++ components/Marginalia/MarginaliaPopup.jsx
@@ -0,0 +1,63 @@
+import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
+import styles from './marginalia.module.css'
+import { useEffect } from 'react'
+import useTheme from '../../hooks/useThemes'
+
+
+
+export default function MarginaliaPopup({ id, username, content, picture, mode, setPopupOpen }) {
+	const [theme, toggleTheme] = useTheme()
+
+	return (
+		<div key={id} className={`${styles.popup} ${theme}`}>
+			<button className={styles.close} onClick={() => setPopupOpen(false)}>Close</button>
+			<p className='marginalia_text'>{content}</p>
+			{picture && (
+				<div className='marginalia_picture'>
+					<Image src={picture} alt='marginalia picture' width={900} height={900}/>
+				</div>
+			)}
+			<p className='marginalia_username'>- {username}</p>
+			{/* </div> */}
+
+			<style jsx global>{`
+				/*
+* Prefixed by https://autoprefixer.github.io
+* PostCSS: v8.4.14,
+* Autoprefixer: v10.4.7
+* Browsers: last 4 version
+*/
+
+				
+
+				.marginalia_username {
+					font-size: 1.2rem;
+					font-family: var(--modern-font);
+				}
+
+				.marginalia_text {
+					font-size: 1.2rem;
+					font-family: var(--modern-font);
+				}
+
+				.marginalia_picture {
+					width: 100%;
+					// overflow: hidden;
+				}
+				@media screen and (max-width: 450px) {
+					.marginalia {
+						width: 12em;
+						height: 14em;
+						margin: 0.5em;
+						/* background-color: white; */
+						-webkit-filter: drop-shadow(0px 0px 3px #5a58cb);
+						filter: drop-shadow(0px 0px 3px #5a58cb);
+						-webkit-backdrop-filter: blur(3px);
+						padding: 20px;
+					}
+				}
+			`}</style>
+		</div>
+	)
+}
