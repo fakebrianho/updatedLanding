2e7a7f4612661941bec6c3dde8d069424284ed05
Brian Ho <bh1525@nyu.edu>
2024-11-10 04:04:08 -0600

    cleaned

--- .history/src/app/navigation/[chapter]/page_20241110035451.js
+++ .history/src/app/navigation/[chapter]/page_20241110035451.js
@@ -6,7 +6,6 @@ export default async function Page({ params }) {
 	const chapter = params.chapter
 	const post = await getDataContent(chapter)
 	console.log('params', params)
-	console.log('posts', post)
 	if (post && post.length > 0) {
 		const serializedNodes = post[0].child_nodes.map((node) => {
 			node._id = JSON.parse(JSON.stringify(node._id))
